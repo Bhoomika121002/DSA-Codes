846de81495b20df8dbce9c82339c09d60af763dc
Bhoomika121002 <chaudharybhoomika12@gmail.com>
2023-06-29 15:55:42 +0530

    commit

--- recursionstrings/removeafromstring.java
+++ recursionstrings/removeafromstring.java
@@ -3,6 +3,9 @@ package recursionstrings;
 public class removeafromstring {
     public static void main(String[] args) {
         skip("","baccdah");
+        System.out.println(skip2("baarfgtah"));
+        System.out.println(skipApple("baapplerfgtah"));
+        System.out.println(skipAppNotApple("baapplerfgtapph"));
     }
 
     //p = processed & up = unprocessed
@@ -36,5 +39,31 @@ public class removeafromstring {
             //current con
         }
     }
-     
+
+    //skip an string 
+     static String skipApple(String up){
+        if(up.isEmpty()){      //base condition 
+            return "";
+        }
+
+        if(up.startsWith("apple")){
+            return skipApple(up.substring(5));
+        }else{
+            return up.charAt(0) + skipApple(up.substring(1));
+
+        }
+    }
+     static String skipAppNotApple(String up){
+        if(up.isEmpty()){      //base condition 
+            return "";
+        }
+
+        if(up.startsWith("app") && !up.startsWith("apple")){
+            return skipAppNotApple(up.substring(3));
+        }else{
+            return up.charAt(0) + skipAppNotApple(up.substring(1));
+
+        }
+    }
 }
+
