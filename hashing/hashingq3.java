b5d724d725faa6dc5df7bf83105076d98a5d1d08
Bhoomika121002 <chaudharybhoomika12@gmail.com>
2023-07-07 02:51:47 +0530

    commit

--- hashing/hashingq3.java
+++ hashing/hashingq3.java
@@ -2,10 +2,41 @@
 
 package hashing;
 
+import java.util.HashMap;
+
 public class hashingq3 {
     public static void main(String[] args) {
+        HashMap<String,String> tickets = new HashMap<>();
+        tickets.put("Chennai","Bengaluru");
+        tickets.put("Mumbai","Delhi");
+        tickets.put("Goa","Chennai");
+        tickets.put("Delhi","Goa");
         
+        String start = getStart(tickets);
+
+        while(tickets.containsKey(start)){
+            System.out.println(start);
+            start = tickets.get(start);
+        }
+        System.out.println(start);
     }
-    
+
+    public static String getStart(HashMap<String,String> tick){
+        HashMap<String,String> revMap = new HashMap<>();
+        //key became value & value becomes key 
+        for (String key : tick.keySet()){
+            //key -> key :val -> tick.get(key)  == in original 
+            revMap.put(tick.get(key),key);
+        }
+
+        for(String key : tick.keySet()){
+            if(!revMap.containsKey(key)){
+                return key;    //just for finding the start 
+            }
+        }
+        return null;
+
+    }
+
     
 }
