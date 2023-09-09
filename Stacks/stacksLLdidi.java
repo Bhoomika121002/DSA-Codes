7654d1083f1b0f9cb6a843fd872cd7dfeed3bd7b
Bhoomika121002 <chaudharybhoomika12@gmail.com>
2023-09-10 03:37:03 +0530

    commit

--- Stacks/stacksLLdidi.java
+++ Stacks/stacksLLdidi.java
@@ -1,5 +1,7 @@
 package Stacks;
 
+import java.util.Stack;
+
 public class stacksLLdidi {
 
     static class Node{
@@ -12,16 +14,49 @@ public class stacksLLdidi {
     }}
 
     static class stack{
-        public node head;
-        public static void push(int data){
-
+        public static Node head;
+        public static boolean isEmpty(){
+            return head == null;
         }
+        public static void push(int data){
+            Node newNode = new Node(data);
+            if(isEmpty()){
+                head = newNode;
+                return;
+            }
+        newNode.next = head;
+        head = newNode;
+        }
+
+        public static int pop(){
+             if(isEmpty()){
+                return -1;}
+            int top = head.data;
+            head = head.next;
+            return top; 
+        }
+
+        public static int peek() {
+            if(isEmpty()){
+                return -1;}
+                return head.data;
+        }
+        
 
     }
 
 
 
     public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
         
     }
     
