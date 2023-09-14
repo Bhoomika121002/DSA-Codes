3fcd4aa0d8a9814fc8234a2fd558c9874c56ac18
Bhoomika121002 <chaudharybhoomika12@gmail.com>
2023-09-14 23:15:03 +0530

    commit

--- Queue/queueLLdidi.java
+++ Queue/queueLLdidi.java
@@ -56,7 +56,7 @@ public static void main(String[] args) {
     Queue q = new Queue();
         q.add(1);
         q.add(2);
-        q.add(3);
+        q.add(3);   //
 
         while(!q.isEmpty()){
             System.out.println(q.peek());
